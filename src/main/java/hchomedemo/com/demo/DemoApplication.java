9f67b63cd6fef27c6cf3a9c2e5d9290a686afb5f
HenryCWF <69089085+HenryCWF@users.noreply.github.com>
2021-12-27 00:48:39 +0800

    Second commit

--- src/main/java/hchomedemo/com/demo/DemoApplication.java
+++ src/main/java/hchomedemo/com/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Congrats ! Demo app deployed from Henry";
+		return "Congrats ! Demo app deployed from Henry ^_^";
 	}
 
 	public static void main(String[] args) {
